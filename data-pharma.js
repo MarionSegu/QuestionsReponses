330c04c0f9008344de7e4af2b5aea127735dafed
Marion <mamarionv@hotmail.fr>
2025-05-13 16:17:31 +0200

    nouvelles q pharma

--- data-pharma.js
+++ data-pharma.js
@@ -3,4 +3,20 @@ const tableauQRPharma = [
         "question" : "Reine des près",
         "reponse" : "Filipendula ulamaria <br> Partie de drogue : sommités fleuries<br> => douleurs rhumatismales"
     },
+    {
+        "question" : "Hapargophytum",
+        "reponse" : "Hapargophytum Procumbens <br> Partie de drogue : ? <br> => douleurs rhumatismales"
+    },
+    {
+        "question" : "Saule Blanc",
+        "reponse" : "Salix Alba <br> Partie de drogue : ? <br> => douleurs rhumatismales"
+    },
+    {
+        "question" : "Ortie",
+        "reponse" : "Urticaria Dioica <br> Partie de drogue : ? <br> => douleurs rhumatismales + DTQ"
+    },
+    {
+        "question" : "Genèvrier",
+        "reponse" : "Juniperus Communis <br> Partie de drogue : ? <br> => douleurs rhumatismales + DTQ"
+    },
 ]
